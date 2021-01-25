94057d12be2c27410853a198f4a003a33eb347b7
BhumitPatel98 <bhumitpatel58@gmail.com>
2021-01-25 16:20:48 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import web from '../src/images/img2.svg'
+import Common from './Common';
+const Home = () => {
+  return(
+    <>
+        <Common
+        name="Grow your Business with"
+        imgsrc={web}
+        visit="/services"
+        btname="Get Started" />
+    </>
+  );
+}
+
+export default Home
